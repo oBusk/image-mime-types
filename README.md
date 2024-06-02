f08e6ee1d46190c7b6e8153f240ab33106671d8a
Oscar Busk <oscar.busk@gmail.com>
2024-06-02 13:30:31 +0200

    Test MIME types

--- README.md
+++ README.md
@@ -1,36 +1,11 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Testing MIME Types
 
-## Getting Started
+Seems like andriod selects to show a "media cloud selector" over a normal file selector when the MIME type is one that they know is an image. 
 
-First, run the development server:
+If you specify a lot of MIME types, some will be outside of android "image" MIME types, and the file selector will be shown instead of the media cloud selector.
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+| File Picker | Media cloud picker |
+|-|-|
+| ![](./file-picker.png) | ![](./cloud-media.png) |
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+When creating a web app around uploading pictures, the preferred experience is to see your Google Photos instad of being asked to browse your files.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,146 @@
-import Image from "next/image";
+const MIMES = [
+  "image/*",
+
+  "image/aces",
+  "image/avci",
+  "image/avcs",
+  "image/avif",
+  "image/bmp",
+  "image/cgm",
+  "image/dicom-rle",
+  "image/emf",
+  "image/fits",
+  "image/g3fax",
+  "image/gif",
+  "image/heic",
+  "image/heic-sequence",
+  "image/heif",
+  "image/heif-sequence",
+  "image/hej2k",
+  "image/hsj2",
+  "image/ief",
+  "image/jls",
+  "image/jp2",
+  "image/jpeg",
+  "image/jph",
+  "image/jphc",
+  "image/jpm",
+  "image/jpx",
+  "image/jxr",
+  "image/jxra",
+  "image/jxrs",
+  "image/jxs",
+  "image/jxsc",
+  "image/jxsi",
+  "image/jxss",
+  "image/ktx",
+  "image/ktx2",
+  "image/png",
+  "image/prs.btif",
+  "image/prs.pti",
+  "image/sgi",
+  "image/svg+xml",
+  "image/t38",
+  "image/tiff",
+  "image/tiff-fx",
+  "image/vnd.adobe.photoshop",
+  "image/vnd.airzip.accelerator.azv",
+  "image/vnd.dece.graphic",
+  "image/vnd.djvu",
+  "image/vnd.dvb.subtitle",
+  "image/vnd.dwg",
+  "image/vnd.dxf",
+  "image/vnd.fastbidsheet",
+  "image/vnd.fpx",
+  "image/vnd.fst",
+  "image/vnd.fujixerox.edmics-mmr",
+  "image/vnd.fujixerox.edmics-rlc",
+  "image/vnd.microsoft.icon",
+  "image/vnd.ms-modi",
+  "image/vnd.ms-photo",
+  "image/vnd.net-fpx",
+  "image/vnd.pco.b16",
+  "image/vnd.tencent.tap",
+  "image/vnd.valve.source.texture",
+  "image/vnd.wap.wbmp",
+  "image/vnd.xiff",
+  "image/vnd.zbrush.pcx",
+  "image/webp",
+  "image/wmf",
+  "image/x-3ds",
+  "image/x-cmu-raster",
+  "image/x-cmx",
+  "image/x-freehand",
+  "image/x-icon",
+  "image/x-jng",
+  "image/x-mrsid-image",
+  "image/x-ms-bmp",
+  "image/x-pcx",
+  "image/x-pict",
+  "image/x-portable-anymap",
+  "image/x-portable-bitmap",
+  "image/x-portable-graymap",
+  "image/x-portable-pixmap",
+  "image/x-rgb",
+  "image/x-tga",
+  "image/x-xbitmap",
+  "image/x-xpixmap",
+  "image/x-xwindowdump",
+
+  "video/*",
+
+  "video/3gpp",
+  "video/3gpp2",
+  "video/h261",
+  "video/h263",
+  "video/h264",
+  "video/iso.segment",
+  "video/jpeg",
+  "video/jpm",
+  "video/mj2",
+  "video/mp2t",
+  "video/mp4",
+  "video/mpeg",
+  "video/ogg",
+  "video/quicktime",
+  "video/vnd.dece.hd",
+  "video/vnd.dece.mobile",
+  "video/vnd.dece.pd",
+  "video/vnd.dece.sd",
+  "video/vnd.dece.video",
+  "video/vnd.dvb.file",
+  "video/vnd.fvt",
+  "video/vnd.mpegurl",
+  "video/vnd.ms-playready.media.pyv",
+  "video/vnd.uvvu.mp4",
+  "video/vnd.vivo",
+  "video/webm",
+  "video/x-f4v",
+  "video/x-fli",
+  "video/x-flv",
+  "video/x-m4v",
+  "video/x-matroska",
+  "video/x-mng",
+  "video/x-ms-asf",
+  "video/x-ms-vob",
+  "video/x-ms-wm",
+  "video/x-ms-wmv",
+  "video/x-ms-wmx",
+  "video/x-ms-wvx",
+  "video/x-msvideo",
+  "video/x-sgi-movie",
+  "video/x-smv",
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      {MIMES.map((mime) => (
+        <div className="p-4" key={mime}>
+          <h4>{mime}</h4>
+          <input type="file" accept={mime} />
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      ))}
     </main>
   );
 }
